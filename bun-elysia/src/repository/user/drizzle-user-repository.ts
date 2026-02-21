df959548025a49f25921045736a22f62fd09ce35
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-02-21 02:30:16 -0300

    feat: adiciona métodos de buscar usuário por ID ou Email

--- bun-elysia/src/repository/user/drizzle-user-repository.ts
+++ bun-elysia/src/repository/user/drizzle-user-repository.ts
@@ -1,13 +1,33 @@
-import type { UserRepository } from "./user-repository";
+import type { User, UserRepository } from "./user-repository";
 import { db } from "@/db/client";
 import { users } from "@/db/schema";
-
 import { userCreateRequestDTO } from "@/dto/users-dtos";
+import { eq } from "drizzle-orm";
 
 export class DrizzleUserRepository implements UserRepository{
   async create(data: userCreateRequestDTO){
     const [ inserted ] = await db.insert(users).values(data).$returningId()
 
     return inserted.id
+  }
+
+  async findById(id: number): Promise<User | null> {
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, id))
+      .limit(1)
+
+    return user ?? null
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1)
+
+    return user ?? null
   }
 }
--- bun-elysia/src/repository/user/user-repository.ts
+++ bun-elysia/src/repository/user/user-repository.ts
@@ -1,5 +1,10 @@
+import { users } from "@/db/schema"
 import { userCreateRequestDTO } from "@/dto/users-dtos"
+
+export type User = typeof users.$inferSelect
 
 export interface UserRepository{
   create(data: userCreateRequestDTO):Promise<number>
+  findById(id: number): Promise<User | null>
+  findByEmail(email: string): Promise<User | null>
 }
